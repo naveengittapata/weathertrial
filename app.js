865765160831b1bd3696436360caccbe4607255c
Naveen Gittapata <naveen.asn11@gmail.com>
2022-03-05 16:51:06 -0600

    third

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var weatherRouter = require('./routes/weathers');
+var weatherRouter = require('./routes/weather');
 
 
 var app = express();
@@ -23,7 +23,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/api/v1/weathers', weatherRouter);
+app.use('/api/v1/weather', weatherRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
