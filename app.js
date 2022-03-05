9184c8b1eff5595f37b3b0122911fce70e46f221
Naveen Gittapata <naveen.asn11@gmail.com>
2022-03-05 16:43:48 -0600

    second commit

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/api/v1/patients', weatherRouter);
+app.use('/api/v1/weathers', weatherRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
